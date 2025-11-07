0f013b701c27c61997352b31013bb953dfb08b1f
Tsebang Maduna <tsebangmaduna6@gmail.com>
2025-11-07 17:09:44 +0200

    updated the home page and worked on custom styles

--- js/custom.js
+++ js/custom.js
@@ -3,20 +3,39 @@ console.log("Custom script loaded!");
 
 
 
-
-
 //------------------------------------------  ANIMATION  -----------------------------------------------------//
 	const Cwelcome = document.querySelector('#welcome');
 	if(Cwelcome){
 		new Typed('#welcome', {
-			strings: ["Welcome to my Canvas"], // your texts
+			strings: ["Welcome to my Canvas"], // my texts
 			typeSpeed: 40,
 			backSpeed: 70,
 			loop: false,
 			showCursor: false
 		});
 	}
-		
+	
+	const Cbrand = document.querySelector('#brand');
+	if(Cbrand){
+		new Typed('#brand', {
+			strings: ["Maduna"], // my texts
+			typeSpeed: 200,
+			backSpeed: 30,
+			loop: false,
+			showCursor: false
+		});
+	}
+	
+	const Cunavailable = document.querySelector('#unavailable');
+	if(Cunavailable){
+		new Typed('#unavailable', {
+			strings: ["The contents of this page are currently unavailable, I apologise for the inconvenience. Please see other pages"], // my texts
+			typeSpeed: 60,
+			backSpeed: 30,
+			loop: false,
+			showCursor: true
+		});
+	}
 
 //-----------------------------------------------------------------------------------------------------------//
 
